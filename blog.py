f269f69eb47b6d3035cda52ca9dd8cd30a8db57b
Jenu Patel <jenu.16@icloud.com>
2017-06-19 18:52:21 -0500

    Controller - updated configuration and the login function

--- blog.py
+++ blog.py
@@ -9,6 +9,16 @@ import sqlite3
 
 # configuration
 DATABASE = 'blog.db'
+# User credentials to log-in to access the main blog page
+USERNAME = 'admin'
+PASSWORD = 'admin'
+# Add the SECRET_KEY, which is used for managing user sessions
+# Best-Practice: Use a random key generator to do this
+# for example:
+# import os
+# os.urandom(24) # generates a random string of 24 chars, hard to guess
+SECRET_KEY =
+\x1ef\xf9\xb2\x0e\xa0\xda\xab\xc7\xbc7\x89\x8a\xaf\xba\x1fT\x86Qo?\xbc,\xa1
 
 app = Flask(__name__)
 
@@ -21,9 +31,19 @@ def connect_db():
 
 # maps the url / to the function login()
 # which in turn sets the route to login.html
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or
+        request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Credentials. Please try again.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+    return render_template('login.html', error=error), status_code
 
 # maps the url /main to the function main()
 # which in turn sets the route to main.html
